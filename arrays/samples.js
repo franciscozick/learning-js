d42b7b23a58301cd8b2f20c50bc16b4327346ca2
Francisco Zick <franciscozick@gmail.com>
2019-01-25 15:49:35 -0800

    a lot of stuff

--- arrays/samples.js
+++ arrays/samples.js
@@ -17,3 +17,34 @@ console.log(columOne);
 console.log(columOne);
 columOne.pop();
 console.log(columOne);//removes the last value of the array
+
+let fruits = ["Banana"];
+
+let basket = fruits; //you can set one var equal to an array
+
+basket.push('Apples');// and if you use push on that var you will directly add to the array
+console.log(fruits);
+
+for (var i=0; i < 3; i++){
+    
+    console.log(rows[i]);   
+}// logs all the values in row one at a time, so in three different lines
+
+for (let row of rows){
+    console.log(rows);
+}// this will run all the values at once three times making three lines of the same thing (not what we want right now)
+
+let arr = [1, 2, 3, 4, 5];
+arr[9]= 10;// positions 5-8 will be empty
+arr.length;// shows the length of the array
+arr.length = 0// you can set up a value for the lenght, if you set it up to 0 it clears the array completely
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(matrix[1][2]);
+matrix.push([10, 11, 12]);
+console.log(matrix);
+matrix.pop();
+console.log(matrix);
--- arrays/tictactoe.js
+++ arrays/tictactoe.js
@@ -3,8 +3,46 @@ var columTwo = ['-', 'O', '-'];
 var columThree = ['X', '-', 'O'];
 
 var rows = [columOne, columTwo, columThree];
+for (let row of rows) {
+    let rowText = '';
+    for (let char of row) {
+        rowText += char;
+    }
+    console.log(rowText);
+} // this will run all the values at once three times making three lines of the same thing (not what we want right now)
 
-for (var i=0; i < 3; i++){
-    
-    console.log(rows[i]);   
+function middle(border, filling){
+    var midChar = border
+    for ( var i = 0; i < 15; i++ ){
+        midChar += filling
+    } 
+    midChar += border
+    return midChar
 }
+
+
+
+let colums = [
+    []
+]
+
+function pushArray(Pushed, number ){
+    for (var i = 0; i < number ; i++){
+    colums.push(Pushed);
+    }
+}
+function popArray(Pushed, number){
+   Pushed
+    for (var i = 0; i < number; i++){
+        colums.pop();
+    }
+}
+function logArray(Pushed, number){
+    pushArray(Pushed, number);
+    for (var i = 0; i < number; i++){
+        console.log(colums[i]);
+    }
+    popArray(Pushed, number);
+    }
+
+logArray(middle('+', '-',), 15);
